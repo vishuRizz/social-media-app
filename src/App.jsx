bc15ef5a2129686930fb43dd2f2e121e6702aae8
vishu pratap <vishurizz01@gmail.com>
2024-06-08 17:02:39 +0530

    UI at basic is done

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,19 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import Sidebar from './components/Sidebar'
 import CreatePost from './components/CreatePost'
-import Post from './components/Post'
+import PostList from './components/PostList'
 
 function App() {
+
+  const [selectedTab, setSelectedTab] = useState('Home');
 
   return (
   
     <div className='app-container'>
-   <Sidebar/>
+    <Sidebar/>
    <div className="content">
    <Header/>
-   <CreatePost/>
-   <Post/>
+   {selectedTab === 'Home' ? (<PostList/>) : (<CreatePost/>)}
    <Footer/>
    </div>
    </div>
---
+++ src/components/PostList.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Post from './Post'
+
+function PostList() {
+  return (
+    <div className='post-list'>
+      <Post/>
+      <Post/>
+      <Post/>
+      <Post/>
+      <Post/>
+    </div>
+  )
+}
+
+export default PostList
