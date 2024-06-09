65cd64a54db12d78a9d80d23827ace2b1bcd0e72
vishu pratap <vishurizz01@gmail.com>
2024-06-09 19:21:36 +0530

    done

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { MdDelete } from "react-icons/md";
+import {PostListData } from '../store/Post-list-store';
 
-function Post() {
+function Post({post}) {
+ const {deletePost} = useContext(PostListData)
   return (
     <div>
-      <div class="card" style = {{width : "18rem"}}>
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+      <div className="custom-card card">
+  <div className="card-body">
+    <h2 className="card-title">
+      {post.title}
+      <span className="top-0 position-absolute start-100 translate-middle badge rounded-pill bg-danger"
+      onClick={ () => deletePost(post.id) } >
+      <MdDelete/>
+  </span>
+      </h2>
+    <p className="card-text">{post.body}</p>
+    <div className='tags-container'>
+    {post.tags.map((tag) => ( <span key={tag} className=" badge text-bg-info custom-tags">{tag}</span> ))}
+    </div>
+    <div class="alert alert-success reactions" role="alert">
+ People reacted: {post.reactions}
+</div>
   </div>
 </div>
     </div>
--- src/store/Post-list-store.jsx
+++ src/store/Post-list-store.jsx
@@ -6,8 +6,17 @@ import { createContext, useReducer } from "react";
   deletePost: () => {},
 });
 
-const postListReducer = (currentPostList, action) => {
-    return currentPostList;
+const postListReducer = (currPostList, action) => {
+  let newPostList = currPostList;
+//  console.log(action.payload.postId)
+  if (action.type === 'DELETE_ITEM') {
+    console.log(action.payload.postId)
+    newPostList = currPostList.filter(
+      (post) => post.id !== action.payload.postId
+    );
+  }
+
+    return currPostList;
 }
 
 const PostListProvider = ({ children }) => {
@@ -16,8 +25,13 @@ const PostListProvider = ({ children }) => {
  const addPost=()=>{ 
 
  }
- const deletePost=()=>{
-
+ const deletePost=(postId)=>{
+ 
+  dispatchPostList({
+    type: 'DELETE_ITEM',
+    payload:{
+    postId,
+  }})
  }
 
   return <PostListData.Provider value={{
@@ -28,19 +42,19 @@ const PostListProvider = ({ children }) => {
 };
 const DEFAULT_POST_LIST =[ {
     id: 1,
-    title: 'going to mumbain',
-    body: 'i am vishu and going to mumbai for vacations', 
+    title: 'Going to mumbain',
+    body: 'i am vishu and going to mumbai for vacations, ill be going to many more places like goa and other places in maharashtra and also will try to go to hydrabad which is in telengana.', 
     reactions: 2,
     userId: 'user-9',
     tags:["vacations", "trip"],
 },
 {
     id: 2,
-    title: 'going to college',
+    title: 'Going to college',
     body: 'learnign react and advance concepts of react like contex api and useReducer', 
     reactions: 15,
     userId: 'user-12',
-    tags:["college", "react"],
+    tags:["college", "react","learning"],
 }
 ]
 
